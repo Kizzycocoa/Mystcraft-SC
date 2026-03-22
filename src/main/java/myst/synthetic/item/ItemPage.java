5380bde698a7555922a6d592ce6bc7cd9a79c7c1
Kizzycocoa <kizzycocoa@yahoo.co.uk>
2026-03-22 17:37:48 +0000

    Auto snapshot -03-2026 17-37

--- src/main/java/myst/synthetic/item/ItemPage.java
+++ src/main/java/myst/synthetic/item/ItemPage.java
@@ -1,10 +1,6 @@
 package myst.synthetic.item;
 
-import java.util.Collection;
-import org.jetbrains.annotations.Nullable;
-
 import myst.synthetic.page.Page;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
@@ -28,44 +24,5 @@ public class ItemPage extends Item {
         }
 
         return Component.translatable("item.mystcraft-sc.page.blank");
-    }
-
-    @Override
-    public void appendHoverText(
-            ItemStack stack,
-            TooltipContext context,
-            net.minecraft.world.item.component.TooltipDisplay tooltipDisplay,
-            java.util.function.Consumer<Component> textConsumer,
-            net.minecraft.world.item.TooltipFlag flag
-    ) {
-        Identifier symbol = Page.getSymbol(stack);
-        if (symbol != null) {
-            textConsumer.accept(
-                    Component.translatable("tooltip.mystcraft-sc.page.symbol", symbol.toString())
-                            .withStyle(ChatFormatting.GRAY)
-            );
-        }
-
-        if (Page.isLinkPanel(stack)) {
-            textConsumer.accept(
-                    Component.translatable("tooltip.mystcraft-sc.page.linkpanel")
-                            .withStyle(ChatFormatting.AQUA)
-            );
-
-            @Nullable Collection<String> properties = Page.getLinkProperties(stack);
-            if (properties != null) {
-                for (String property : properties) {
-                    textConsumer.accept(Component.literal(" - " + property).withStyle(ChatFormatting.DARK_AQUA));
-                }
-            }
-        }
-
-        Integer quality = Page.getTotalQuality(stack);
-        if (quality != null && quality > 0) {
-            textConsumer.accept(
-                    Component.translatable("tooltip.mystcraft-sc.page.quality", quality)
-                            .withStyle(ChatFormatting.DARK_GRAY)
-            );
-        }
     }
 }
