5d512054078bdafda8f94f04ae1173d272676b92
Kizzycocoa <kizzycocoa@yahoo.co.uk>
2026-03-31 14:34:59 +0100

    Auto snapshot -03-2026 14-34

--- src/client/java/myst/synthetic/client/render/PageItemSpecialRenderer.java
+++ src/client/java/myst/synthetic/client/render/PageItemSpecialRenderer.java
@@ -1,4 +1,3 @@
-
 package myst.synthetic.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -34,40 +33,39 @@ public final class PageItemSpecialRenderer implements SpecialModelRenderer<PageR
 
         poseStack.pushPose();
 
-        // Establish the page in item-local space first.
-        // The quad itself is centred around the origin (-0.5 .. 0.5),
-        // so we move that origin into normal item model space here.
+        // Centre the page quad in normal item-local space.
         poseStack.translate(0.5F, 0.5F, 0.0F);
 
         switch (itemDisplayContext) {
             case GUI -> {
-                poseStack.scale(0.90F, 0.90F, 0.90F);
+                // Fill the slot properly.
+                poseStack.scale(1.12F, 1.12F, 1.12F);
             }
             case GROUND -> {
-                poseStack.scale(0.55F, 0.55F, 0.55F);
+                poseStack.scale(0.70F, 0.70F, 0.70F);
             }
             case FIXED -> {
-                poseStack.scale(0.90F, 0.90F, 0.90F);
+                poseStack.scale(1.00F, 1.00F, 1.00F);
             }
             case THIRD_PERSON_RIGHT_HAND -> {
                 poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
                 poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
-                poseStack.scale(0.78F, 0.78F, 0.78F);
+                poseStack.scale(0.95F, 0.95F, 0.95F);
             }
             case THIRD_PERSON_LEFT_HAND -> {
                 poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
-                poseStack.scale(0.78F, 0.78F, 0.78F);
+                poseStack.scale(0.95F, 0.95F, 0.95F);
             }
             case FIRST_PERSON_RIGHT_HAND -> {
                 poseStack.mulPose(Axis.YP.rotationDegrees(-35.0F));
-                poseStack.scale(0.88F, 0.88F, 0.88F);
+                poseStack.scale(1.00F, 1.00F, 1.00F);
             }
             case FIRST_PERSON_LEFT_HAND -> {
                 poseStack.mulPose(Axis.YP.rotationDegrees(35.0F));
-                poseStack.scale(0.88F, 0.88F, 0.88F);
+                poseStack.scale(1.00F, 1.00F, 1.00F);
             }
             default -> {
-                poseStack.scale(0.90F, 0.90F, 0.90F);
+                poseStack.scale(1.00F, 1.00F, 1.00F);
             }
         }
 
@@ -94,7 +92,6 @@ public final class PageItemSpecialRenderer implements SpecialModelRenderer<PageR
 
     @Override
     public void getExtents(Consumer<Vector3fc> consumer) {
-        // Extents should match the item-local space after centering.
         consumer.accept(new Vector3f(0.0F, 0.0F, -0.01F));
         consumer.accept(new Vector3f(1.0F, 1.0F, 0.01F));
     }
