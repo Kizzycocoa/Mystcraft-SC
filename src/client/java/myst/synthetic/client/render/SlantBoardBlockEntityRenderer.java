0a99c9ec80f626a7c8ad66841fb7d2b9b96e4f92
Kizzycocoa <kizzycocoa@yahoo.co.uk>
2026-04-05 00:00:00 +0100

    Auto snapshot -04-2026 23-59

--- src/client/java/myst/synthetic/client/render/SlantBoardBlockEntityRenderer.java
+++ src/client/java/myst/synthetic/client/render/SlantBoardBlockEntityRenderer.java
@@ -226,8 +226,8 @@ public class SlantBoardBlockEntityRenderer
                 );
             } else {
                 poseStack.translate(0.0F, 0.20F, 0.0F);
-                poseStack.mulPose(Axis.XP.rotationDegrees(180.0F));
-                //poseStack.mulPose(Axis.YP.rotationDegrees(90.0F));
+                poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
+                poseStack.mulPose(Axis.YP.rotationDegrees(90.0F));
 
                 // These values lift the flat item out of the board after the board-pitch rotation.
                 poseStack.translate(0.0F, 0.32F, 0.01F);
