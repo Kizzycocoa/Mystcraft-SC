e5af80380ed99ff0bbc14219e39ecebd0ef66a7b
Kizzycocoa <kizzycocoa@yahoo.co.uk>
2026-04-05 17:15:02 +0100

    Auto snapshot -04-2026 17-15

--- src/client/java/myst/synthetic/client/render/SlantBoardBlockEntityRenderer.java
+++ src/client/java/myst/synthetic/client/render/SlantBoardBlockEntityRenderer.java
@@ -251,7 +251,7 @@ public class SlantBoardBlockEntityRenderer
                 poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
 
                 // Small nudge so it sits on the board rather than clipping into the slot.
-                poseStack.translate(-0.3F, 0.16F, -0.02F);
+                poseStack.translate(-0.3F, 0.16F, -0.06F);
 
                 poseStack.scale(0.8F, 0.8F, 0.8F);
 
