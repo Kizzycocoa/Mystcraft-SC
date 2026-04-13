3d5a2e9658ca920bbdccceaf8fccff36b56bc670
Kizzycocoa <kizzycocoa@yahoo.co.uk>
2026-04-13 23:06:29 +0100

    Auto snapshot -04-2026 23-06

--- src/client/java/myst/synthetic/client/render/BookstandRenderPipelines.java
+++ src/client/java/myst/synthetic/client/render/BookstandRenderPipelines.java
@@ -34,7 +34,7 @@ public final class BookstandRenderPipelines {
 
         return RenderType.create(
                 name,
-                RenderSetup.builder(RenderPipelines.ENTITY_CUTOUT_NO_CULL_Z_OFFSET)
+                RenderSetup.builder(RenderPipelines.CUTOUT_BLOCK)
                         .withTexture("Sampler0", getTexture(wood))
                         .useLightmap()
                         .createRenderSetup()
